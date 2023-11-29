0a2884bd5ced382fb228f982f76bafe015c94688
Taavi Kalaluka <taavi.k@protonmail.com>
2023-11-29 16:11:47 +0200

    made first_name and last_name required

--- movie_wizard/accounts/serializers.py
+++ movie_wizard/accounts/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(max_length=30, required=True)
+    last_name = serializers.CharField(max_length=30, required=True)
+
     class Meta(object):
         model = User
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name']
