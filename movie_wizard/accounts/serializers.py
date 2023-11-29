02a6d64040b53dd74a9746d996553ff741668e01
Taavi Kalaluka <taavi.k@protonmail.com>
2023-11-29 16:50:42 +0200

    added age validation to registering

--- movie_wizard/accounts/serializers.py
+++ movie_wizard/accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import UserProfile
+from datetime import date, timedelta
 
 class UserSerializer(serializers.ModelSerializer):
     date_of_birth = serializers.DateField(source='userprofile.date_of_birth', required=True)
@@ -11,8 +12,15 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name', 'date_of_birth']
 
+    def validate_date_of_birth(self, value):
+        today = date.today()
+        if value > today - timedelta(days=13*365.25):
+            raise serializers.ValidationError('You must be at least 13 years old to register.')
+        return value
+
     def create(self, validated_data):
-        profile_data = validated_data.pop('userprofile')
+        profile_data = validated_data.pop('userprofile', None)
         user = User.objects.create(**validated_data)
-        UserProfile.objects.create(user=user, **profile_data)
+        if profile_data:
+            UserProfile.objects.create(user=user, **profile_data)
         return user
