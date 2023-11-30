af189c06d0eebe489f71f6202a1a71c772daf112
Taavi Kalaluka <taavi.k@protonmail.com>
2023-11-30 14:59:10 +0200

    created new dataset, with no null values

--- scripts/populate_db.py
+++ scripts/populate_db.py
@@ -21,8 +21,7 @@ with open(csv_file_path, 'r') as file:
     next(reader)
 
     titles_to_create = [Title(title_type=row[0], primary_title=row[1], original_title=row[2], is_adult=row[3],
-                              start_year=int(row[4]) if row[4] != "\\N" else None,
-                              runtime_minutes=int(row[5]) if row[5] != "\\N" else None, genres=row[6]) for row in reader]
+                              start_year=int(row[4]), runtime_minutes=int(row[5]), genres=row[6]) for row in reader]
 
     batch_size = 100000
     title_batches = [titles_to_create[i:i + batch_size] for i in range(0, len(titles_to_create), batch_size)]
