cb1cb09eba1ec5b7573d989117c4f963020c441e
Taavi Kalaluka <taavi.k@protonmail.com>
2023-12-02 19:14:03 +0200

    added back button to header

--- movie_wizard_frontend/src/components/Header.js
+++ movie_wizard_frontend/src/components/Header.js
@@ -1,5 +1,6 @@
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { AppBar, Toolbar, Typography, Button, IconButton } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import axios from "axios";
 
 const Header = () => {
@@ -18,20 +19,30 @@ const Header = () => {
                 );
                 localStorage.removeItem("token");
 
-                // Redirect to login page
                 navigate("/login");
             } catch (error) {
                 console.error("Failed to logout", error);
-                // Handle error
             }
         }
+    };
+
+    const handleBack = () => {
+        navigate(-1); // Go to the previous page
     };
 
     return (
         <AppBar position="static">
             <Toolbar>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    onClick={handleBack}
+                    sx={{ mr: 2 }}
+                >
+                    <ArrowBackIcon />
+                </IconButton>
                 <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                    My App
+                    Movie Wizard
                 </Typography>
                 <Button color="inherit" onClick={handleLogout}>
                     Logout
