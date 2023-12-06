995359c325d788f5378a3246d4251245800c10f1
Taavi Kalaluka <taavi.k@protonmail.com>
2023-12-07 01:05:06 +0200

    added tests for deleting reviews

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Movie Wizard :movie_camera: :mage: :crystal_ball:
 
+<div align="center>
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![Django](https://img.shields.io/badge/django-%23092E20.svg?style=for-the-badge&logo=django&logoColor=white)
+![DjangoREST](https://img.shields.io/badge/DJANGO-REST-ff1709?style=for-the-badge&logo=django&logoColor=white&color=ff1709&labelColor=gray)
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
+![Azure](https://img.shields.io/badge/azure-%230072C6.svg?style=for-the-badge&logo=microsoftazure&logoColor=white)
+
+</div>
+
 #### **Still in development. Finishing touches and deployment will be done over the next few days :eyes: **
 
 ### Overview
--- movie_wizard/reviews/tests.py
+++ movie_wizard/reviews/tests.py
@@ -166,7 +166,7 @@ class UpdateReviewTest(APITestCase):
             'email': 'testuser@example.com',
             'first_name': 'testuser',
             'last_name': 'testuser',
-            'date_of_birth': '2010-10-1' # 13-year-old user to test age restriction
+            'date_of_birth': '1990-01-01'
         }
         serializer = UserSerializer(data=user_data)
         if serializer.is_valid():
@@ -252,4 +252,78 @@ class UpdateReviewTest(APITestCase):
             'text': 'Updated Content'
         }
         response = self.client.put(f"{self.update_review_url}?review_id={other_review.id}", data)
+        self.assertEqual(response.status_code, 403)
+
+class DeleteReviewTest(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        user_data = {
+            'username': 'testuser',
+            'password': 'testpassword123',
+            'email': 'testuser@example.com',
+            'first_name': 'testuser',
+            'last_name': 'testuser',
+            'date_of_birth': '1990-01-01'
+        }
+        serializer = UserSerializer(data=user_data)
+        if serializer.is_valid():
+            serializer.save()
+            self.user = User.objects.get(username=user_data['username'])
+            self.user.set_password(user_data['password'])
+            self.user.save()
+        else:
+            raise Exception(serializer.errors)
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.title = Title.objects.create(
+            title_type='Movie',
+            primary_title='Test Title',
+            original_title='Test Title',
+            is_adult=False,
+            start_year=2000,
+            runtime_minutes=120,
+            genres='Action,Adventure',
+            average_review=2.5
+        )
+        self.review = Review.objects.create(user=self.user, title=self.title, rating=5, text='Test Content')
+        self.delete_review_url = '/api/reviews/remove'
+
+    def test_delete_review_with_valid_review_id(self):
+        response = self.client.delete(f"{self.delete_review_url}?review_id={self.review.id}")
+        self.assertEqual(response.status_code, 204)
+        self.assertFalse(Review.objects.filter(id=self.review.id).exists())
+
+    def test_delete_review_with_invalid_review_id(self):
+        response = self.client.delete(f"{self.delete_review_url}?review_id=9999")
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_review_without_review_id(self):
+        response = self.client.delete(self.delete_review_url)
+        self.assertEqual(response.status_code, 400)
+
+    def test_delete_review_without_authentication(self):
+        self.client.logout()
+        response = self.client.delete(f"{self.delete_review_url}?review_id={self.review.id}")
+        self.assertEqual(response.status_code, 403)
+
+    def test_delete_review_without_permission(self):
+        user_data = {
+            'username': 'otheruser',
+            'password': 'otherpassword123',
+            'email': 'otheruser@example.com',
+            'first_name': 'other',
+            'last_name': 'testuser',
+            'date_of_birth': '1999-02-09',
+        }
+        serializer = UserSerializer(data=user_data)
+        if serializer.is_valid():
+            serializer.save()
+            otheruser = User.objects.get(username=user_data['username'])
+            otheruser.set_password(user_data['password'])
+            otheruser.save()
+        else:
+            raise Exception(serializer.errors)
+        
+        other_review = Review.objects.create(user=otheruser, title=self.title, rating=3, text="Other user's review")
+        response = self.client.delete(f"{self.delete_review_url}?review_id={other_review.id}")
         self.assertEqual(response.status_code, 403)
